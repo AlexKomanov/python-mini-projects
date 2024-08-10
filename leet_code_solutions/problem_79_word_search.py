bbd60eae1e0c98ccba3bc2001b3601536a83327a
AlexKomanov <akomanov88@gmail.com>
2024-08-10 16:07:40 +0300

    Adding problem 79 solution

---
+++ leet_code_solutions/problem_79_word_search.py
@@ -0,0 +1,61 @@
+from typing import List
+
+
+# https://leetcode.com/problems/word-search/description/
+
+# board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]] # board example
+class Solution:
+    def dfs(self, row: int, col: int, idx: int) -> bool:
+        if idx == len(self.word):
+            return True
+
+        key = row, col
+
+        if (
+                row < 0
+                or
+                col < 0
+                or
+                row >= self.rows
+                or
+                col >= self.cols
+                or
+                self.word[idx] != self.board[row][col]
+                or
+                key in self.paths
+        ):
+            return False
+
+        self.paths.add(key)
+
+        res = False
+        for r, c in (
+                (-1, 0),
+                (1, 0),
+                (0, -1),
+                (0, 1),
+        ):
+            if self.dfs(row + r, col + c, idx + 1):
+                res = True
+                break
+        self.paths.remove(key)
+        return res
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.rows = len(board)
+        self.cols = len(board[0])
+        self.board = board
+        self.word = word
+        self.paths = set()
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.dfs(row, col, 0):
+                    return True
+        return False
+
+
+board_1 = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+word_1 = "ABCB"
+solution = Solution()
+is_existing = solution.exist(board_1, word_1)
+print(is_existing)
